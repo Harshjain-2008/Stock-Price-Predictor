a3a7d04ab63dc2fa3b4465228275c0cc053d9664
Harshjain-2008 <indian123om@gmail.com>
2026-06-17 01:00:46 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,15 +1,17 @@
+import pandas as pd
+
 def predict_future_price(model, latest_row):
 
-    prediction = model.predict([
-        latest_row["Open"],
-        latest_row["High"],
-        latest_row["Low"],
-        latest_row["Volume"],
-        latest_row["Day"],
-        latest_row["Month"],
-        latest_row["Year"]
+    input_df = pd.DataFrame({
+        "Open": [latest_row["Open"]],
+        "High": [latest_row["High"]],
+        "Low": [latest_row["Low"]],
+        "Volume": [latest_row["Volume"]],
+        "Day": [latest_row["Day"]],
+        "Month": [latest_row["Month"]],
+        "Year": [latest_row["Year"]]
+    })
 
-    ])
+    prediction = model.predict(input_df)
 
     return prediction[0]
-
